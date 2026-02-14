4b651d307363e84989a19dc8881f45ff5b5da78f
MansiRana3 <ranamansi2101@gmail.com>
2026-02-14 12:11:32 +0530

    Update API URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://eventify-backend-r0s3.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
